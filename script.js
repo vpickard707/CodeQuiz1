7c286b4e0ac142b2da398d76c626062636e09bf2
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2020-10-24 20:12:57 -0700

    testing

--- script.js
+++ script.js
@@ -10,6 +10,74 @@ clear.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
 });
+
+//Questions
+const myQuestions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
+    }
+  ];
+  //-------------------------------------------------------------
+  function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
+  
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+  
+        // variable to store the list of possible answers
+        const answers = [];
+  
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
+  
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+  
+        // add this question and its answers to the output
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+  
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
 // retrieves local storage
 var allScores = localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
@@ -27,6 +95,7 @@ if (allScores !== null) {
 // Event listener to move to index page
 goBack.addEventListener("click", function () {
     window.location.replace("./index.html");
-});
+}
+);
 
 
